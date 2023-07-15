4a0af80d917c38c5ec97245b9cb97aab096351df
Oleg Svettsov <svettsoff_oleg@mail.ru>
2023-07-15 13:58:33 +0300

    Созданы таблицы Product, Category. Создан admin.

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    discription = models.TextField()
+    price = models.FloatField()
+    category_id = models.IntegerField()
+
+class Product(models.Model):
+    category_id = models.IntegerField()
+    name = models.CharField(max_length=200)
