6dd4ebb4176f10aebbc16fc0581990145b97f29b
Oleg Svettsov <svettsoff_oleg@mail.ru>
2023-07-22 13:08:23 +0300

    Добавлена и заполнена таблица БЖУ

--- shop/main/models.py
+++ shop/main/models.py
@@ -5,7 +5,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     discription = models.TextField()
     price = models.FloatField()
+    link_foto = models.TextField(default='link_foto')
     category_id = models.IntegerField()
+
+
+
 
     def __str__(self):
         return self.name
@@ -18,6 +22,8 @@ class Product(models.Model):
 class Category(models.Model):
     category_id = models.IntegerField()
     name = models.CharField(max_length=200)
+    discription = models.TextField(default='discription')
+    link_foto = models.TextField(default='link_foto')
 
     def __str__(self):
         return self.name
@@ -25,3 +31,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+class ProductCalories(models.Model):
+    name = models.CharField(max_length=200, default='name')
+    calories = models.IntegerField()
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    carbohydrates = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'БЖУ'
+        verbose_name_plural = 'БЖУ'
+
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category, Product
+from .models import Category, Product, ProductCalories
 
 
 def main(request):
@@ -14,9 +14,11 @@ def about(response):
 def category(response, id_cat):
     data = Product.objects.filter(category_id=id_cat)
     cat_name = Category.objects.get(category_id=id_cat)
-    return render(response, 'main/category.html', {'data': data, 'name': cat_name.name})
+    return render(response, 'main/category.html',
+                  {'data': data, 'name': cat_name.name, 'discription': cat_name.discription})
 
 
 def product(response, id_prod):
     data = Product.objects.get(id=id_prod)
-    return render(response, 'main/product.html', {'product': data})
+    calories = ProductCalories.objects.get(id=id_prod)
+    return render(response, 'main/product.html', {'product': data, 'calories': calories})
